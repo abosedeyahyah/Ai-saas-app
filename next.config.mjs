e21774f1abbdcb36024e73685371d31c5c02a367
abosedeyahyah <abosedeyahyah@gmail.com>
2024-09-14 06:23:12 +0800

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
   },
-  output:"export"
 };
 
 export default nextConfig;
